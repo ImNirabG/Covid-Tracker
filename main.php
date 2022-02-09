d1194005b3a86f14ad74a31578ec232b8d67f882
ImNirabG <98900495+ImNirabG@users.noreply.github.com>
2022-02-09 11:37:52 -0500

    Update main.php

--- main.php
+++ main.php
@@ -23,9 +23,7 @@ echo'
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="container">
-        <h1>Thank you for contacting us. We will get back to you as soon as possible!</h1
-        
+  
     </div>
 </body>
 </html>
