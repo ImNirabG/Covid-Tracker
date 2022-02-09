d452c3424f3e56816afb09471303e91bdb158c39
ImNirabG <98900495+ImNirabG@users.noreply.github.com>
2022-02-09 13:45:13 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -42,11 +42,3 @@ if($_POST) {
     .'Content-type: text/html; charset=utf-8' . "\r\n"
     .'From: ' . $visitor_email . "\r\n";
       
-    if(mail($recipient, $email_title, $email_body, $headers)) {
-        echo "<p>Thank you for contacting us, $visitor_name. You will get a reply within 24 hours.</p>";
-    } else {
-        echo '<p>We are sorry but the email did not go through.</p>';
-    }
-      
-} else {
-    echo '<p>Something went wrong</p>';
