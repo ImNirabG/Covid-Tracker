33aceec305b52a96c9da7fc8b81c71a6b0a20d78
ImNirabG <98900495+ImNirabG@users.noreply.github.com>
2022-02-09 11:33:48 -0500

    Update main.php

--- main.php
+++ main.php
@@ -24,8 +24,7 @@ echo'
 </head>
 <body>
     <div class="container">
-        <h1>Thank you for contacting me. I will get back to you as soon as possible!</h1>
-        <p class="back">Go back to the <a href="index.html">homepage</a>.</p>
+        <h1>Thank you for contacting us. We will get back to you as soon as possible!</h1
         
     </div>
 </body>
